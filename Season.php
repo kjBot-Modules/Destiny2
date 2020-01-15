28d84637e5ad3b0c74b32dcc5d49de61d883cfba
kj415j45 <kj415j45@gmail.com>
2020-01-15 15:14:05 +0800

    Initial commit

---
+++ Season.php
@@ -0,0 +1,14 @@
+<?php
+namespace kjBotModule\kj415j45\Destiny2;
+
+use kjBot\Framework\Module;
+use kjBot\Framework\Event\MessageEvent;
+use kjBot\SDK\CQCode;
+
+class Season extends Module{
+    public function process(array $args, MessageEvent $event){
+        return $event->sendBack(
+            CQCode::Image('https://www.bungie.net/pubassets/pkgs/130/130375/ag_season9_calendar_16x9_TW.jpg')
+        );
+    }
+}
