17f1304d82123428ce84d733548d72866eb9d824
kj415j45 <kj415j45@gmail.com>
2020-02-02 14:11:11 +0800

    Beautify

--- Weekly.php
+++ Weekly.php
@@ -300,8 +300,8 @@ class Weekly extends Module{
 
     private function getRaid($image = false){
         $raid = '';
-        $raid.= $this->getLewiathan($image);
-        $raid.= $this->getGardenOfSalvation($image);
+        $raid.= trim($this->getLewiathan($image));
+        $raid.= trim($this->getGardenOfSalvation($image));
         $raid.= trim($this->getClanRAID($image));
 
         return static::GenerateWeeklyFragment('RAID', [
